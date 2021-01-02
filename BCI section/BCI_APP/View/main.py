5e34fb09295d9994bc48df2ddb6f5d40c62d99dd
baraamagdy <baraamagdy2004@gmail.com>
2021-01-02 17:27:50 +0200

    append boxes

--- BCI section/BCI_APP/View/main.py
+++ BCI section/BCI_APP/View/main.py
@@ -1,14 +1,11 @@
-"""
-
-"""
-import sys,os
-sys.path.append(os.path.abspath(os.path.join('..', 'BCI')))
-from  PyQt5.QtWidgets import QApplication
+import sys
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget # it's a base class of all user insterface objects in PyQt 
 from PyQt5.uic import loadUiType 
 from os import path
 import sys
-from Controller.DataCollecting import Collect
+from exp_senario2 import expScenario
+ 
 
 OUR_UI= loadUiType(path.join(path.dirname(__file__),'MainLayout.ui'))[0]
 # return tuple of child and base class  
@@ -17,17 +14,20 @@ class OurAPP(QWidget,OUR_UI):
     def __init__(self,parent=None):
         super(OurAPP,self).__init__(parent)
         QWidget.__init__(self)
-        self.dc = Collect()
         self.setupUi(self)
-        self.TrainButton_Handling()
+        self.RecordButton_Handling()
         self.back_handling()
         self.initiators()
+        self.sc1= expScenario()
+        self.stacklayout.insertWidget(1,self.sc1)
 
-    def TrainButton_Handling(self):
-        self.TrainButton.clicked.connect(lambda : self.stacklayout.setCurrentIndex(1))
+    def RecordButton_Handling(self):
+        self.RecordButton.clicked.connect(lambda : self.stacklayout.setCurrentIndex(1))
 
     def back_handling(self):
         self.BackButton.clicked.connect(lambda : self.stacklayout.setCurrentIndex(0))    
+
+
 
     def initiators(self):
         self.setWindowTitle('Data Processing-Train-Prediction APP')
@@ -43,6 +43,5 @@ def main():
     sys.exit(app.exec_()) # terminate system when window is terminated 
 
 if __name__ == '__main__':
-    # print(sys.path)
     main()
     # print(OUR_UI)
