fd22500a2e337dea488ea4dedc70c33d84c2cd12
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-01-02 02:13:07 +0200

    View

---
+++ BCI section/BCI_APP/View/main.py
@@ -0,0 +1,44 @@
+"""
+
+"""
+import sys
+from  PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget # it's a base class of all user insterface objects in PyQt 
+from PyQt5.uic import loadUiType 
+from os import path
+import sys
+
+OUR_UI= loadUiType(path.join(path.dirname(__file__),'MainLayout.ui'))[0]
+# return tuple of child and base class  
+
+class OurAPP(QWidget,OUR_UI):
+    def __init__(self,parent=None):
+        super(OurAPP,self).__init__(parent)
+        QWidget.__init__(self)
+        self.setupUi(self)
+        self.TrainButton_Handling()
+        self.back_handling()
+        self.initiators()
+
+    def TrainButton_Handling(self):
+        self.TrainButton.clicked.connect(lambda : self.stacklayout.setCurrentIndex(1))
+
+    def back_handling(self):
+        self.BackButton.clicked.connect(lambda : self.stacklayout.setCurrentIndex(0))    
+
+    def initiators(self):
+        self.setWindowTitle('Data Processing-Train-Prediction APP')
+        self.Username_le.setPlaceholderText('User Name')
+        self.labelseq_le.setPlaceholderText('label seq e.g. 10,6.5,..')
+        self.dp_le.setPlaceholderText('Data Points number')
+        self.Tflash_le.setPlaceholderText('Flashing Time')
+        
+def main():
+    app = QApplication(sys.argv)
+    window = OurAPP()
+    window.show()
+    sys.exit(app.exec_()) # terminate system when window is terminated 
+
+if __name__ == '__main__':
+    main()
+    # print(OUR_UI)
