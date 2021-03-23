72d1deec8983cf6342c02b2fe47f1b73721729a2
Eman2554 <59311924+Eman2554@users.noreply.github.com>
2021-03-23 19:14:29 +0200

    Add files via upload

---
+++ Motor_config.h
@@ -0,0 +1,23 @@
+/*****************************************************/
+/* Auther : Eman Mohamed                             */
+/* Date   : 23 March 2021                            */
+/* Version: V01                                      */
+/*****************************************************/
+
+#ifndef MOTOR_CONFIG_H
+#define MOTOR_CONFIG_H
+
+#define MOTOR1_PORT   GPIOD
+#define MOTOR2_PORT		GPIOB
+
+/*PORT And PINs For Motor A */
+#define M1INA 	PIN0
+#define M1INB 	PIN1
+#define M1PWM  	PIN2
+
+/*PORT And PINs For Motor A */
+#define M2INA  PIN0
+#define M2INB  PIN1
+#define M2PWM  PIN2
+
+#endif
---
+++ Motor_interface.h
@@ -0,0 +1,17 @@
+/*****************************************************/
+/* Auther : Eman Mohamed                             */
+/* Date   : 23 March 2021                            */
+/* Version: V01                                      */
+/*****************************************************/
+
+#ifndef MOTOR_INTERFACE_H
+#define MOTOR_INTERFACE_H
+
+void HMOTOR_voidInit(void);
+void HMOTOR_voidTurnRight(void);
+void HMOTOR_voidTurnLeft(void);
+void HMOTOR_voidStop(void);
+void HMOTOR_voidForward(void);
+void HMOTOR_voidMotorSpeed(void);
+
+#endif 
