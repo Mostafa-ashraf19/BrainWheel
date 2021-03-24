071391f618e5feaeae39870996b2a50dd9fd6584
Eman2554 <59311924+Eman2554@users.noreply.github.com>
2021-03-24 19:23:40 +0200

    Add files via upload

---
+++ EmbeddedSys/LIB/BIT_MATH.h
@@ -0,0 +1,16 @@
+/*****************************************************/
+/* Auther : Eman Mohamed                             */
+/* Date   : 29 JUl 2020                              */
+/* Version: V01                                      */
+/*****************************************************/
+
+#ifndef BIT_MATH_H
+#define BIT_MATH_H
+
+#define SET_BIT(VAR,BIT)          VAR |=  (1 << (BIT))
+#define CLR_BIT(VAR,BIT)          VAR &= ~(1 << (BIT))
+#define GET_BIT(VAR,BIT)          ((VAR >> BIT) & 1  )
+#define TOG_BIT(VAR,BIT)          VAR ^=  (1 << (BIT))
+
+
+#endif
