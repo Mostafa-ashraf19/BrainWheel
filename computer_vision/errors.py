6769ce3eb3b8ef11ed5cabe7f957c28aad5e90e6
Pierre Nabil <pipopierrenabil@gmail.com>
2021-03-24 15:29:29 +0200

    Delete computer_vision directory

--- computer_vision/errors.py
+++
@@ -1,9 +0,0 @@
-""" Exceptions module for Computer Vision Class.
-"""
-
-class CVException(Exception):
-	pass
-
-class CameraNotConnectedError(CVException):
-	def __str__(self):
-		return 'ZED camera is not connected.'
