9101bb24129cbbdc160536cb94b8e9cb3ecb55c9
girgis_micheal <girgismicheal00@yahoo.com>
2021-05-30 03:24:10 +0200

    Update mainproccess.py

--- mainproccess.py
+++ mainproccess.py
@@ -10,14 +10,26 @@ Pca_Name = 'Finalized_PCA_model.sav'
 
 
 class Process:
+    '''
+    this class responsible for BCI real time response
+    '''
 
     def __init__(self):
+        """
+        - Load Pretrained Model & Pca
+        - open serial communication with jetson
+        """
         # print ('process is created')
         self.Model = pk.load(open(Model_Name, 'rb'))
         self.pca = pk.load(open(Pca_Name, 'rb'))
         # self.serial_sender = SerialSender()
 
     def _predict(self, Feature):
+        """
+        private function
+        :param Feature: the Feature vector of the data
+        :return: Model prediction
+        """
         Feature = pd.DataFrame(self.pca.transform(Feature))
         p = np.array(self.Model.decision_function(Feature))  # decision is a voting function
         prob = np.exp(p) / np.sum(np.exp(p), axis=1, keepdims=True)  # softmax after the voting
@@ -25,6 +37,12 @@ class Process:
         return self.Model.predict(Feature)
 
     def _prepare_data(self, Data):
+        """
+        the function pass Real time Data through \
+        signal processing pipeline (CAR -> Filter -> Welch -> Feature Extraction block)
+        :param Data: data recorded by the head set in n seconds
+        :return: Feature vector
+        """
         # print('main process')
         # print(Data)
         # applay car DF -> Df
@@ -40,17 +58,25 @@ class Process:
         return Features
 
     def _send_to_jetson(self, prediction):
+        """
+        this function sends prediction to jetson due to serial communication port
+        :param prediction: the prediction of the model
+        """
         # map prediction to characters
         print(prediction)
         char = {7.5: 'forward', 8.57: 'right', 10.0: 'left', 12.0: 'stop'}
         # characters to bus
-
         # self.serial_sender.send_inst(char[prediction])
 
     # preds = predict(x_train[:5], model)
     # print(preds)
 
     def make_process(self, Data):
+        """
+        the interface function with the Gui
+        :param Data: data recorded sent from GUi
+        :return: prediction to Jetson
+        """
         # print ('data is sent')
         freq = {0: 7.5, 1: 8.57, 2: 10.0, 3: 12.0}
         # print (Data)
@@ -63,19 +89,19 @@ class Process:
         self._send_to_jetson(freq[prediction[0]])
 
 
-from BCInterface.Preprocessing.DataPrepare import DataPrepare
-import glob
-
-if __name__ == "__main__":
-    number_sec = 5  # sellect example time
-    path_dir_ = 'C:/Users/compuland/Documents/GitHub/BCI_interface/EEG-SSVEP-DataSet/mike/mike 2/'
-    File_regexp = 'U0000[c|d|e|f]*.csv'
-    prepare = DataPrepare()
-    data = prepare.get_dataFromFiles(glob.glob(path_dir_ + File_regexp), concate=True)
-    Labels = data['Label']  # .drop('Label',axis=1)
-
-    Run_time = Process()
-
-    example = 0
-    print(data.iloc[example * 640:example * 640 + 640, :-1])
-    Run_time.make_process(data.iloc[example * 640:example * 640 + 640, :-1])
+# from BCInterface.Preprocessing.DataPrepare import DataPrepare
+# import glob
+#
+# if __name__ == "__main__":
+#     number_sec = 5  # sellect example time
+#     path_dir_ = 'C:/Users/compuland/Documents/GitHub/BCI_interface/EEG-SSVEP-DataSet/mike/mike 2/'
+#     File_regexp = 'U0000[c|d|e|f]*.csv'
+#     prepare = DataPrepare()
+#     data = prepare.get_dataFromFiles(glob.glob(path_dir_ + File_regexp), concate=True)
+#     Labels = data['Label']  # .drop('Label',axis=1)
+#
+#     Run_time = Process()
+#
+#     example = 0
+#     print(data.iloc[example * 640:example * 640 + 640, :-1])
+#     Run_time.make_process(data.iloc[example * 640:example * 640 + 640, :-1])
